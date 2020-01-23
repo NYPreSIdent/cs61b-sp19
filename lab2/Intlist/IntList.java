d210f6f949f78df5143e8a299bc48b0906ab5701
ShouZY <liaosong546263@gmail.com>
2020-01-23 18:31:34 +0800

    update null argument feature.

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,8 +82,10 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        if (A == null || B == null) {
-            return null;
+        if (A == null){
+            return B;
+        } else if (B == null) {
+            return A;
         }
         IntList p = A;
         while (p.rest != null) {
@@ -98,8 +100,10 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        if (A == null || B == null) {
-            return null;
+        if (A == null){
+            return B;
+        } else if (B == null) {
+            return A;
         }
         IntList result = new IntList(A.first, null);
         IntList ptr = result;

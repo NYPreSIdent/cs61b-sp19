d695d175c355c7cda2d4546b330132f4912d8f6a
ShouZY <liaosong546263@gmail.com>
2020-02-16 00:27:40 +0800

    falied version.

--- clab/clab6/BubbleGrid.java
+++ clab/clab6/BubbleGrid.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BubbleGrid {
     private int[][] grid;
 
@@ -13,7 +15,63 @@ public class BubbleGrid {
      * are unique, valid locations in the grid. Must be non-destructive
      * and have no side-effects to grid. */
     public int[] popBubbles(int[][] darts) {
-        // TODO
-        return null;
+        int[] result = new int[darts.length];
+        UnionFind set = new UnionFind(grid.length * grid[0].length);
+        /* union every row bubbles. */
+        unionRow(grid, set);
+        /* union every column bubbles. */
+        unionColumn(grid, set);
+        /* return the result. */
+        return maniDart(result, set, darts);
+    }
+
+    /** union every bubbles on the rows. */
+    private static void unionRow(int[][] a, UnionFind set) {
+        for (int i = 0; i < a.length; i += 1) {
+            int j = 0;
+            while ((j < a[i].length) && (a[i][j] != 1)) {
+                j += 1;
+            }
+            if (j == a[i].length) {
+                continue;
+            }
+            int foundation = i * a[i].length + j;
+            for (int index = j + 1; index < a[i].length; index += 1) {
+                if (a[i][index] == 1) {
+                    set.union(foundation, i * a[i].length + index);
+                }
+            }
+        }
+    }
+
+    /** union every bubbles on the column. */
+    private static void unionColumn(int[][] a, UnionFind set) {
+        for (int i = 0; i < a[0].length; i += 1) {
+            int j = 0;
+            while ((j < a.length) && (a[j][i] == 0)) {
+                j += 1;
+            }
+            if (j >= a.length) {
+                continue;
+            }
+            int foundation = j * a[j].length + i;
+            for (int index = j + 1; index < a.length; index += 1) {
+                if (a[index][i] == 1) {
+                    set.union(foundation, j * a[j].length + index);
+                }
+            }
+        }
+    }
+
+    /** return the result of the arrays. */
+    private int[] maniDart(int[] result, UnionFind set, int[][] dart) {
+        for (int i = 0; i < dart.length; i += 1) {
+            int sum;
+            for (int elem : dart[i]) {
+                sum = dart[i][0] * dart.length + dart[i][1];
+                result[i] = set.sizeOf(sum);
+            }
+        }
+        return result;
     }
 }
--- clab/clab6/UnionFind.java
+++ clab/clab6/UnionFind.java
@@ -1,35 +1,42 @@
 public class UnionFind {
-
-    // TODO - Add instance variables?
+    private int[] parents;
+    private int[] size;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
+        parents = new int[n];
+        size = new int[n];
+        for (int i = 0; i < n; i += 1) {
+            parents[i] = i;
+            size[i] = 1;
+        }
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        if (vertex > parents.length || vertex < 0) {
+            throw new RuntimeException("Out of index.");
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        int root = parent(v1);
+        return size[root];
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        return -1;
+        validate(v1);
+        return parents[v1];
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
+        return find(v1) == find(v2);
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -38,14 +45,34 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        int parentOfv1 = find(v1);
+        int parentOfv2 = find(v2);
+
+        if (sizeOf(parentOfv1) > sizeOf(parentOfv2)) {
+            parents[parentOfv2] = parentOfv1;
+            size[parentOfv1] += sizeOf(parentOfv2);
+        } else {
+            parents[parentOfv1] = parentOfv2;
+            size[parentOfv2] += sizeOf(parentOfv1);
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO
-        return -1;
+        validate(vertex);
+        int dad = vertex;
+        while(parent(dad) != dad) {
+            dad = parent(dad);
+        }
+
+        while (vertex != dad) {
+            int temp = parent(vertex);
+            parents[vertex] = dad;
+            vertex = temp;
+        }
+
+        return dad;
     }
 
 }

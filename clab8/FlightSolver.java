7d21fdf740483d4c87427a73a76798ebc2185d21
ShouZY <liaosong546263@gmail.com>
2020-03-07 16:22:24 +0800

    Solver finished.

--- clab8/FlightSolver.java
+++ clab8/FlightSolver.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * Solver for the Flight problem (#9) from CS 61B Spring 2018 Midterm 2.
@@ -7,14 +9,37 @@ import java.util.ArrayList;
  * considered to be in the air at the same time.
  */
 public class FlightSolver {
+    private ArrayList<Flight> Flights;
+    private PriorityQueue<Flight> StartQueue;
+    private PriorityQueue<Flight> EndQueue;
 
     public FlightSolver(ArrayList<Flight> flights) {
-        /* FIX ME */
+        this.Flights = flights;
+        Comparator<Flight> startTimeComparator = Comparator.comparingInt(f -> f.startTime);
+        Comparator<Flight> EndTimeComparator = Comparator.comparingInt(f -> f.endTime);
+        StartQueue = new PriorityQueue<>(Flights.size(), startTimeComparator);
+        EndQueue = new PriorityQueue<>(Flights.size(), EndTimeComparator);
     }
 
     public int solve() {
-        /* FIX ME */
-        return -1;
+        StartQueue.addAll(Flights);
+        EndQueue.addAll(Flights);
+        int current = 0;
+        int temp = 0;
+        while (StartQueue.peek() != null) {
+            Flight start = StartQueue.peek();
+            Flight end = EndQueue.peek();
+            if (start.startTime <= end.endTime) {
+                temp += start.passengers;
+                StartQueue.remove();
+            } else {
+                temp -= end.passengers;
+                EndQueue.remove();
+            }
+            if (temp > current) {
+                current = temp;
+            }
+        }
+        return current;
     }
-
 }

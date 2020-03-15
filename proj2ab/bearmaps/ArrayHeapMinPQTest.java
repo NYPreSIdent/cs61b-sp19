0bf957eb6e2dbe6e6cd66962e6a42ef067f65055
ShouZY <liaosong546263@gmail.com>
2020-03-15 11:22:51 +0800

    proj 2A finished. :

--- proj2ab/bearmaps/ArrayHeapMinPQTest.java
+++ proj2ab/bearmaps/ArrayHeapMinPQTest.java
@@ -1,7 +1,77 @@
 package bearmaps;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 public class ArrayHeapMinPQTest {
+    /** test the function of add items. */
+    @Test
+    public void addTest() {
+        MinPQ<String> testCase = new MinPQ<>();
+        testCase.add("hello", 2.0);
+        testCase.add("the", 3.0);
+        testCase.add("world", 4.0);
+        testCase.add("go", 5.0);
+        for (double i = 5.0; i < 1000; i += 1) {
+            testCase.add("a" + (int) i, i);
+        }
+
+        assertEquals("hello", testCase.getSmallest());
+    }
+
+    @Test
+    public void deleteTest() {
+        MinPQ<String> testCase = new MinPQ<>();
+        testCase.add("hello", 2.0);
+        testCase.add("the", 3.0);
+        testCase.add("world", 4.0);
+        testCase.add("go", 1.0);
+        for (double i = 11; i < 80; i += 1) {
+            testCase.add("a" + (int) i, i);
+        }
+
+        for (int j = 11; j < 80; j += 1) {
+            testCase.removeSmallest();
+        }
+
+        assertEquals("a76", testCase.getSmallest());
+    }
+
+    @Test
+    public void ContainsTest() {
+        MinPQ<String> testCase = new MinPQ<>();
+        testCase.add("hello", 2.0);
+        testCase.add("the", 3.0);
+        testCase.add("world", 4.0);
+        testCase.add("go", 5.0);
+        assertTrue(testCase.contains("go"));
+
+        for (int i = 0; i < 4; i += 1) {
+            testCase.removeSmallest();
+        }
+
+        assertFalse(testCase.contains("go"));
+    }
+
+    @Test
+    public void changePriorityTest() {
+        MinPQ<String> testCase = new MinPQ<>();
+        testCase.add("hello", 2.0);
+        testCase.add("the", 3.0);
+        testCase.add("world", 4.0);
+        testCase.add("go", 5.0);
+        assertTrue(testCase.contains("go"));
+
+        testCase.changePriority("go", 1.0);
+        testCase.changePriority("world", 1.5);
+        assertEquals("go", testCase.removeSmallest());
+
+        for (double i = 11; i < 80; i += 1) {
+            testCase.add("a" + (int) i, i);
+            testCase.changePriority("a" + (int) i, 1000 - i);
+        }
+        assertEquals("world", testCase.removeSmallest());
+    }
 }
--- proj2ab/bearmaps/TimingTestDemo.java
+++ proj2ab/bearmaps/TimingTestDemo.java
@@ -9,21 +9,31 @@ import edu.princeton.cs.algs4.Stopwatch;
  */
 public class TimingTestDemo {
     public static void main(String[] args) {
-        long start = System.currentTimeMillis();
-        int sum = 0;
-        for (int i = 0; i < 100000; i += 1) {
-            for (int j = 0; j < 10000; j += 1) {
-                sum = sum + i + j;
-            }
+        MinPQ<String> testCase = new MinPQ<>();
+        testCase.add("hello", 2.0);
+        testCase.add("the", 3.0);
+        testCase.add("world", 4.0);
+        testCase.add("go", 5.0);
+        for (int i = 0; i < 1000000; i += 1) {
+            testCase.add("a" + i, i);
         }
-        long end = System.currentTimeMillis();
-        System.out.println("Total time elapsed: " + (end - start)/1000.0 +  " seconds.");
+        Stopwatch sw1 = new Stopwatch();
+        for (int i = 0; i < 1000; i += 1) {
+            testCase.getSmallest();
+        }
+        System.out.println("Total time elapsed: " + sw1.elapsedTime() +  " seconds.");
 
+        NaiveMinPQ<String> testCase2 = new NaiveMinPQ<>();
+        testCase2.add("hello", 2.0);
+        testCase2.add("the", 3.0);
+        testCase2.add("world", 4.0);
+        testCase2.add("go", 5.0);
+        for (int i = 0; i < 1000000; i += 1) {
+            testCase2.add("a" + i, i);
+        }
         Stopwatch sw = new Stopwatch();
-        for (int i = 0; i < 100000; i += 1) {
-            for (int j = 0; j < 10000; j += 1) {
-                sum = sum + i + j;
-            }
+        for (int i = 0; i < 1000; i += 1) {
+            testCase2.getSmallest();
         }
         System.out.println("Total time elapsed: " + sw.elapsedTime() +  " seconds.");
     }
